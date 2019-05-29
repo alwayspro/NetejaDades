e46b3b398cb6eefa13b0c3b4314ff8971f317058
aballonix <49390714+aballonix@users.noreply.github.com>
2019-05-29 15:44:08 +0200

    Create pac2.py

---
+++ pac2.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+plt.rc("font", size=14)
+import seaborn as sns
+sns.set(style="white") #fons millor blanc
+sns.set(style="whitegrid", color_codes=True)
